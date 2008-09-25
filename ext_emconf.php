a0e2972979570e98872a54c153c45d5b1e69f49a
commodore <commodore@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-25 08:46:36 +0000

    Share project "ast_countdown" into "https://svn.typo3.org/TYPO3v4/Extensions/ast_countdown".
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ast_countdown/trunk@12297 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "ast_countdown"
+#
+# Auto generated 06-03-2008 15:33
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'AST Countdown',
+	'description' => 'Dispalys a graphical countdown to a given date. Output format is (d)dd:hh:ii:ss. You can select if you want to show rather two or three digits for the days.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Andre Steiling',
+	'author_email' => 'steiling@elemente.ms',
+	'author_company' => 'elemente websolutions',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '1.2.3',
+	'_md5_values_when_last_written' => 'a:26:{s:12:"ext_icon.gif";s:4:"346a";s:17:"ext_localconf.php";s:4:"3799";s:14:"ext_tables.php";s:4:"9170";s:28:"ext_typoscript_constants.txt";s:4:"205a";s:24:"ext_typoscript_setup.txt";s:4:"1384";s:15:"flexform_ds.xml";s:4:"158a";s:13:"locallang.xml";s:4:"c990";s:16:"locallang_db.xml";s:4:"2867";s:17:"locallang_tca.xml";s:4:"31b9";s:14:"doc/manual.sxw";s:4:"d706";s:33:"pi1/class.tx_astcountdown_pi1.php";s:4:"3890";s:26:"pi1/tx_astcountdown_pi1.js";s:4:"21aa";s:17:"pi1/digits/c0.gif";s:4:"bdb3";s:17:"pi1/digits/c1.gif";s:4:"e4e7";s:17:"pi1/digits/c2.gif";s:4:"d8e2";s:17:"pi1/digits/c3.gif";s:4:"b625";s:17:"pi1/digits/c4.gif";s:4:"034e";s:17:"pi1/digits/c5.gif";s:4:"65bd";s:17:"pi1/digits/c6.gif";s:4:"daf3";s:17:"pi1/digits/c7.gif";s:4:"46bf";s:17:"pi1/digits/c8.gif";s:4:"2255";s:17:"pi1/digits/c9.gif";s:4:"686e";s:17:"pi1/digits/ct.gif";s:4:"8744";s:18:"pi1/digits/ctd.gif";s:4:"8744";s:18:"pi1/digits/cth.gif";s:4:"8744";s:18:"pi1/digits/ctm.gif";s:4:"8744";}',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
