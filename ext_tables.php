45ffcea1c98c8746560e4855ee6de45d3dd4ce0d
commodore <commodore@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-05 10:02:17 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ast_countdown/trunk@14615 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -7,7 +7,7 @@
 	$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages,recursive';
 	// Flex rein
 	$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
-	// Flexform Schema enth‰lt
+	// Flexform Schema enth√§lt
 	t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds.xml');
 	t3lib_extMgm::addPlugin(Array('LLL:EXT:ast_countdown/locallang_db.php:tt_content.list_type_pi1', $_EXTKEY.'_pi1'), 'list_type');
 	t3lib_extMgm::addStaticFile($_EXTKEY, 'pi1/static/', 'Countdown');
